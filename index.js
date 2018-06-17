3d65acb164b7b2c58c9284aac617108cac285f72
EdPutans <edputans@gmail.com>
2018-06-17 22:34:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(n1,n2) {
+  return n1+n2
+}
+
+function subtract (n1,n2) {
+  return n1-n2
+}
+
+function divide(n1,n2){
+  return n1/n2
+}
+
+function multiply(n1,n2) {
+  return n1*n2
+}
+
+
+function inc(n) {
+  return n + 1
+}
+
+function dec(n) {
+  return n-1
+}
+
+; fuction makeInt(n) {
+  return parseInt(n)
+}
